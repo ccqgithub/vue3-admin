eb3aca79f92245550201c9365392603aa7052ef6
season.chen <season.chen.i@foxmail.com>
2022-07-02 14:46:37 +0800

    chore: update config

--- vite.config.ts
+++ vite.config.ts
@@ -5,53 +5,54 @@ import legacy from '@vitejs/plugin-legacy';
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import ElementPlus from 'unplugin-element-plus/vite';
 import { createClassNamehash } from './scripts/utils/createClassNameHash';
-import { getEnvs } from './scripts/utils/getEnvs';
+import { loadEnv } from './scripts/utils/loadEnv';
 
 const root = __dirname;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    ...getEnvs(),
-    __VUE_I18N_LEGACY_API__: JSON.stringify('false'),
-    __VUE_I18N_FULL_INSTALL__: JSON.stringify('false'),
-    __INTLIFY_PROD_DEVTOOLS__: JSON.stringify('false')
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-      '~@': path.resolve(__dirname, './src/'),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
-    }
-  },
-  css: {
-    modules: {
-      generateScopedName: function (name, filename, css) {
-        return createClassNamehash({
-          root,
-          name,
-          filename,
-          css,
-          prefix: 't'
-        });
+export default defineConfig((ctx) => {
+  const { define, envDir, envPrefix } = loadEnv({ mode: ctx.mode, root });
+
+  return {
+    envDir,
+    envPrefix,
+    define,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/'),
+        '~@': path.resolve(__dirname, './src/'),
+        'vue-i18n': 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
       }
+    },
+    css: {
+      modules: {
+        generateScopedName: function (name, filename, css) {
+          return createClassNamehash({
+            root,
+            name,
+            filename,
+            css,
+            prefix: 't'
+          });
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      vueI18n(),
+      ElementPlus(),
+      legacy({
+        targets: [
+          'chrome 87',
+          'safari 13',
+          'firefox 78',
+          'edge 88',
+          'electron 14'
+        ]
+      })
+    ],
+    build: {
+      target: ['es2015']
     }
-  },
-  plugins: [
-    vue(),
-    vueI18n(),
-    ElementPlus(),
-    legacy({
-      targets: [
-        'chrome 87',
-        'safari 13',
-        'firefox 78',
-        'edge 88',
-        'electron 14'
-      ]
-    })
-  ],
-  build: {
-    target: ['es2015']
-  }
+  };
 });
